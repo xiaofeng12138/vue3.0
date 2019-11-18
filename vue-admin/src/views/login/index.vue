f57d8595e2517e592476c09294e8e6c775137de6
xiaofeng <760811650@qq.com>
2019-11-18 08:52:19 +0800

    构建项目

---
+++ vue-admin/src/views/login/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="login">
+        这是一个登录的页面
+         <div>
+            <el-button>默认按钮</el-button>
+            <el-button type="primary">主要按钮</el-button>
+            <el-button type="success">成功按钮</el-button>
+            <el-button type="info">信息按钮</el-button>
+            <el-button type="warning">警告按钮</el-button>
+            <el-button type="danger">危险按钮</el-button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scope>
+    .login{
+        font-size: 30px
+    }
+
+</style>
